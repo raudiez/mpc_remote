25950c3b1480463feedc19f5f92a66dc0c74aff0
Raúl Díez Sánchez <rauldiez20@gmail.com>
2016-04-14 16:21:29 +0200

    mpc_remote with mode.

--- src/mpc_remote.py
+++ src/mpc_remote.py
@@ -10,28 +10,56 @@ CMD_MPC_NEXT="mpc next"
 
 sockid = lirc.init("mpd_remote", blocking=False)
 
-status = os.popen(CMD_CHK_MPC_STATE).read()
 playing = False
-if status == "playing": playing = True
+mpc_mode = False
 
+def chk_playing_status():
+	global playing
+	status = os.popen(CMD_CHK_MPC_STATE).read()
+	if status == "playing":
+		playing = True
+	else :
+		playing = False
+
+chk_playing_status()
 while True:
 	try:
+		global playing
 	  button = lirc.nextcode()
 	  if len(button) == 0: continue
-	  print(button[0])
-	  if button[0] == "KEY_NEXT":
+	  if button[0] == "KEY_NEXT" and mpc_mode:
+	  	chk_playing_status()
+	  	if not playing:
+	  		playing=True
 	  	os.system(CMD_MPC_NEXT)
-	  if button[0] == "KEY_PREVIOUS":
+	  if button[0] == "KEY_PREVIOUS" and mpc_mode:
+	  	chk_playing_status()
+	  	if not playing:
+	  		playing=True
 	  	os.system(CMD_MPC_PREV)
-	  if button[0] == "KEY_PLAYPAUSE":
+	  if button[0] == "KEY_PLAYPAUSE" and mpc_mode:
+	  	chk_playing_status()
 	  	if playing:
 	  		os.system(CMD_MPC_PAUSE)
 	  		playing=False
 	  	else:
 	  		os.system(CMD_MPC_PLAY)
 	  		playing=True
+	  if button[0] == "KEY_MODE":
+	  	if mpc_mode:
+	  		mpc_mode = False
+	  		os.system("systemctl start mediacenter")
+	  		os.system(CMD_MPC_PAUSE)
+	  	else:
+	  		mpc_mode = True
+	  		os.system("systemctl stop mediacenter")
+	  		if playing:
+	  			os.system(CMD_MPC_PLAY)
 
 	  time.sleep(1)
 	except KeyboardInterrupt:
 	  lirc.deinit()
 	  break
+
+
+KEY_MODE
